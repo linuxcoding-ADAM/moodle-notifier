56b0be7c33f629332c446370b3ab0f4227e5f446
BlackCore <the-gray-hat-1414@kali.kali>
2025-10-29 19:01:40 +0100

    Add executable start.sh and update Procfile for Railway

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export CHROMEDRIVER_PATH=$(which chromedriver)
-echo "--- Starting Application ---"
-echo "Found chromedriver at: $CHROMEDRIVER_PATH"
-echo "--------------------------"
-python moodle_checker.py
